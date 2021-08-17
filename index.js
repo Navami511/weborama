5fa511b154947f454866df88ad744d23258b6c7d
Navami511 <navamiajay@gmail.com>
2021-08-17 20:04:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let a=0
+function add(){
+    a++
+    document.getElementById('screen').innerText=a
+
+}
+function sub(){
+    a--
+    document.getElementById('screen').innerText=a
+
+}
+
+function reset(){
+    document.getElementById('screen').innerText=' '
+}
